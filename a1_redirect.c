6bf9a3fbcd01ef81afbc61139781a4f4a9ff5c1a
Serge Jabo Byusa <jabo.byusa@mail.mcgill.ca>
2018-02-01 02:55:32 -0500

    Needs to be run

--- a1_redirect.c
+++ a1_redirect.c
@@ -10,28 +10,27 @@ int main(int argc, char *argv[])
     
     int  fileDesc;
     int ret;
+    
+    printf("First : Print to stdout \n");
     fileDesc = open("redirectout.txt", O_RDWR | O_CREAT| O_APPEND); //create  or append or writeonly
     if(fileDesc < 0)
     {
         perror("There is an error in opening the file");
         exit(1);
     }
-    int copyDesc = dup(fileDesc);
-    write(copyDesc, "Second : Print to redirectout.txt \n", 46);
-    //ret = return
-    ret = dup2(fileDesc,1);//at position 1 for out
-    
-    if(ret < 0)
+    int copyStdOut= dup(1); //Takes whatever it to the next available position
+    if( copyStdOut< 0)
     {
         perror("there is an error in dup2");
         exit(1);
     }
-    //system("ls /tmp");
+    dup2(fileDesc, 1); //then 2 goes to 1
+    printf("Second : Print to redirectout.txt \n");
+    dup2(copyStdOut,1);//at position 1 for out
+    close(copyStdOut);
+    printf("Third : Print to stdout \n");
     close(fileDesc);
-    //printf("First : Print to stdout \n");
-    //..........done......printf("Second : Print to redirectout.txt \n");
-    //printf("Third : Print to stdout \n");
-
+    
     return 0;
 }
 
