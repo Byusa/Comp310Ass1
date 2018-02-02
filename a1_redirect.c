05ccdbd0bced2a673a9d3b43c4bdff899a23b0c2
Serge Jabo Byusa <jabo.byusa@mail.mcgill.ca>
2018-02-01 19:08:11 -0500

    Done with Q1

--- a1_redirect.c
+++ a1_redirect.c
@@ -1,4 +1,12 @@
+/*File Name: a1_redirection.java
+Developers: <<Serge Jabo Byusa>>
+Student ID: 2060612575
+Purpose: <<Redirecting the output to redirectout.txt>>
+*/
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
 #include <unistd.h>
 
 int main(int argc, char *argv[])
@@ -8,27 +16,32 @@ int main(int argc, char *argv[])
     int ret;
     
     printf("First : Print to stdout \n");
-    fileDesc = open("redirectout.txt", O_RDWR | O_CREAT| O_APPEND); //create  or append or writeonly
-    if(fileDesc < 0)
+
+    fileDesc = open("redirectout.txt", O_RDWR | O_CREAT); //open and create or readwrite
+    if(fileDesc < 0) //checking for openning fileDescriptor errors
     {
         perror("There is an error in opening the file");
         exit(1);
     }
-    int copyStdOut= dup(1); //Takes whatever it to the next available position
-    if( copyStdOut< 0)
+
+    int copyStdOut= dup(1); //copy whatever that is in 1 and save it in copyStdOut
+    if( copyStdOut< 0) 
     {
         perror("there is an error in dup2");
         exit(1);
     }
-    dup2(fileDesc, 1); //then 2 goes to 1
-    printf("Second : Print to redirectout.txt \n");
-    dup2(copyStdOut,1);//at position 1 for out
-    close(copyStdOut);
-    printf("Third : Print to stdout \n");
+
+    dup2(fileDesc, 1); //then keep filedescriptors in position 1
+
+    printf("Second : Print to redirectout.txt \n"); //print it in out at position 1 in that file
+
+    dup2(copyStdOut,1); //put back what was in 1(out)
+
+    close(copyStdOut); //close what was in 1 and filedescriptor
     close(fileDesc);
+
+    printf("Third : Print to stdout \n");
+    
     
     return 0;
 }
-
-
-
